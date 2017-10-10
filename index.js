c62184db6f62fcb31a13197eaf6fbe27695e0f5f
z.tran.hoanvu <z.tran.hoanvu@gree.net>
2017-10-10 16:25:40 +0700

    Init project

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js - place code in here for IOS !!!
+
+// Import library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+  <Header />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
