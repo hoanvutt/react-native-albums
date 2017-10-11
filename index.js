f83417bc33d06065de931cdaafe0eef3a74e01bc
z.tran.hoanvu <z.tran.hoanvu@gree.net>
2017-10-11 15:16:32 +0700

    Commit code

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 // Import library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,31 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#E8E8E8',
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
   }
 };
 
